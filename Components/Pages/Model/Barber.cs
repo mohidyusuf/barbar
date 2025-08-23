5e9873e104cbb81a72119e609a98f6ea6b559bc0
mohidyusuf <mohidyusuf02@gmail.com>
2025-08-23 20:03:32 +0500

    Initial commit with pipeline

---
+++ Components/Pages/Model/Barber.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjectBarber.Components.Pages.Model
+{
+    public class Barber
+    {
+        public int Id { get; set; }
+        public string? ServiceName { get; set; }
+        public string? Category { get; set; }  // e.g., Haircut, Beard, Facial
+        public decimal Price { get; set; }
+
+        public string? ImageUrl { get; set; } // Path to image
+    }
+
+    public class SelectedService
+    {
+        public Barber? Service { get; set; }
+        public int Quantity { get; set; } = 1;
+    }
+
+    
+        public class CartItemDto
+        {
+            public string? ServiceName { get; set; }
+            public int Quantity { get; set; }
+        }
+    
+
+}
